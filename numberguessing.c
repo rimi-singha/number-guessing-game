6fedfa5fa4fa23d41465e36a1624f8dbf8d4b7e5
rimisingha86-hue <rimisingha86@gmail.com>
2026-04-04 19:11:25 +0530

    Added number guessing game in C

---
+++ numberguessing.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int random_num;
+
+    srand(time(0)); // Seed random generator
+
+    random_num = rand() % 100 + 1; // Random number 1 to 100
+
+    // printf("Random number: %d", num);
+    int number_of_guessing = 0;
+    int guessed_num;
+    do
+    {
+        printf("guess the number:");
+        scanf("%d", &guessed_num);
+        number_of_guessing++;
+        if (guessed_num > random_num)
+            printf("heigher\n");
+        else if (guessed_num < random_num)
+        {
+            printf("lower\n");
+        }
+        else
+            printf("congrats!!🎉\n");
+    } while (guessed_num!= random_num);
+    printf("you guessed %d times", number_of_guessing);
+    return 0;
+}
